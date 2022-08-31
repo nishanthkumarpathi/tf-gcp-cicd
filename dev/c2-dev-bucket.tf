cac8336a81412c5574924976c67b5c24c4c88351
Nishanth <pathinishanth@gmail.com>
2022-08-31 20:46:28 +0300

    fixed region

--- dev/c2-dev-bucket.tf
+++ dev/c2-dev-bucket.tf
@@ -2,7 +2,7 @@ module "creatingdevbucket" {
     source = "../modules/bucket"
     
     m_bucket_name = "apple"
-    m_bucket_location = "us-east-1"
+    m_bucket_location = "us-east1"
     m_environment_name = "dev"
 }
 
